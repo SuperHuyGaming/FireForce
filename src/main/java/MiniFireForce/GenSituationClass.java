98a49af04343d337be8709c7454041adcd748845
u1485085 <nghkhang2005@gmail.com>
2025-02-21 23:20:03 -0700

    writing getter and setter for fireStation and activeFire

--- src/main/java/MiniFireForce/GenSituationClass.java
+++ src/main/java/MiniFireForce/GenSituationClass.java
@@ -1,4 +1,30 @@
 package MiniFireForce;
-
+import java.sql.Array;
+import  java.util.*;
 public class GenSituationClass {
+    private List<Fire> activeFire;
+    privare List<FireStation> fireStations;
+
+    public GenSituationClass(){
+        this.activeFire = new ArrayList<>();
+        this.fireStations = new ArrayList<>();
+    }
+
+    public List<Fire> getActiveFire(){
+        return activeFire;
+    }
+
+    public void setActiveFire(List<Fire> activeFire){
+        this.activeFire = activeFire;
+    }
+
+    public List<FireAction> fireStations(){
+        return fireStations;
+    }
+
+    public void fireStation(){
+        this.fireStations = fireStations;
+    }
+
+
 }
