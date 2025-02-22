fc2fc0967eccad24c1fdbad6df63d26026fffffc
u1485085 <nghkhang2005@gmail.com>
2025-02-22 14:29:31 -0700

    retrieve fire station

--- src/main/java/MiniFireForce/FireStation.java
+++ src/main/java/MiniFireForce/FireStation.java
@@ -4,7 +4,11 @@ public class FireStation {
     private float xStation;
     private float yStation;
     private int trucks;
-    private static int id = 0;
+
+    // private PriorityQueue<Integer> queue;
+    private static int id;
+
+    // Take the location algorithm of Google Maps to estimate the distance
 
     public FireStation(float xStation, float yStation, int trucks) {
         this.xStation = xStation;
@@ -13,22 +17,37 @@ public class FireStation {
         id++;
     }
 
+    //Getters
     public int getID() {
-        return this.id;
+        return id;
     }
+
+    public float getX() {
+        return xStation;
+    }
+
+    public float getY() {
+        return yStation;
+    }
+
+    public int getTrucks() {
+        return trucks;
+    }
+
     public double calculateDistance(float xFire, float yFire) {
         return Math.sqrt((xFire - xStation) * (xFire - xStation) + (yFire - yStation) * (yFire - yStation));
     }
 
-    public boolean canDeploy() {
-        return trucks > 0;
+    public boolean canDeploy(int requiredTrucks) {
+        return trucks >= requiredTrucks;
+    }
+
+    public void retrieve(int returnTrucks) {
+        this.trucks += returnTrucks;
     }
 
     public boolean deployTruck(int requiredTrucks) {
-        if (canDeploy()) {
-            if (trucks < requiredTrucks) {
-                trucks = 0;
-            }
+        if (canDeploy(requiredTrucks)) {
             trucks -= requiredTrucks;
             return true;
         }
