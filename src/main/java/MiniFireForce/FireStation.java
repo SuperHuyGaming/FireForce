d3b1bfa1ca510e240fc9c888758f86ab82758964
SuperHuyGaming <minhhuyngoctruong@gmail.com>
2025-02-22 16:23:14 -0500

    push FireTruck

--- src/main/java/MiniFireForce/FireStation.java
+++ src/main/java/MiniFireForce/FireStation.java
@@ -4,7 +4,6 @@ public class FireStation {
     private float xStation;
     private float yStation;
     private int trucks;
-
     // private PriorityQueue<Integer> queue;
     private static int id;
 
@@ -17,33 +16,15 @@ public class FireStation {
         id++;
     }
 
-    //Getters
     public int getID() {
-        return id;
+        return this.id;
     }
-
-    public float getX() {
-        return xStation;
-    }
-
-    public float getY() {
-        return yStation;
-    }
-
-    public int getTrucks() {
-        return trucks;
-    }
-
     public double calculateDistance(float xFire, float yFire) {
         return Math.sqrt((xFire - xStation) * (xFire - xStation) + (yFire - yStation) * (yFire - yStation));
     }
 
     public boolean canDeploy(int requiredTrucks) {
         return trucks >= requiredTrucks;
-    }
-
-    public void retrieve(int returnTrucks) {
-        this.trucks += returnTrucks;
     }
 
     public boolean deployTruck(int requiredTrucks) {
---
+++ src/main/java/MiniFireForce/FireTruck.java
@@ -0,0 +1,12 @@
+package MiniFireForce;
+
+public class FireTruck {
+    private static int id;
+    private int velocity;
+    private int extinguishTime;
+
+    public FireTruck(int velocity, int extinguishTime) {
+        
+    }
+
+}
