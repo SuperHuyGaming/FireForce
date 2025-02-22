91dabc59f03cfb5d764d25c79325c9532d595133
u1485085 <nghkhang2005@gmail.com>
2025-02-22 00:04:37 -0700

    implementing generate fire

--- src/main/java/MiniFireForce/GenSituationClass.java
+++ src/main/java/MiniFireForce/GenSituationClass.java
@@ -1,41 +1,42 @@
 package MiniFireForce;
+import java.time.LocalDateTime;
 import  java.util.*;
 
 public class GenSituationClass {
     private List<Fire> activeFire;
-    privare List<FireStation> fireStations;
+    privare List<FireStation>fireStations;
 
-    public GenSituationClass(){
+    public GenSituationClass() {
         this.activeFire = new ArrayList<>();
         this.fireStations = new ArrayList<>();
     }
 
-    public List<Fire> getActiveFire(){
+    public List<Fire> getActiveFire() {
         return activeFire;
     }
 
-    public void setActiveFire(List<Fire> activeFire){
+    public void setActiveFire(List<Fire> activeFire) {
         this.activeFire = activeFire;
     }
 
-    public List<FireAction> fireStations(){
+    public List<FireAction> fireStations() {
         return fireStations;
     }
 
-    public void fireStation(){
+    public void fireStation() {
         this.fireStations = fireStations;
     }
 
+    public void generateFire(){
     Random random = new Random();
-    int x = random.nextInt(-1000,1000);
+    int x = random.nextInt(-1000, 1000);
     int y = random.nextInt(-1000, 1000);
     int severity = random.nextInt(10) + 1;
-    int time = random.nextInt(60);
+    LocalDateTime time = LocalDateTime.now();;
 
-    Fire generateFire = new Fire(x,y,severity,time);
+    Fire generateFire = new Fire(x, y, severity, time);
     activeFire.add(generateFire);
-
-
+}
 
 
 
