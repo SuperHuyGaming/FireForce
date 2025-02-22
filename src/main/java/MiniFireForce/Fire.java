396a5f1aea8ae5b55185affc95f9236f11478f93
SuperHuyGaming <minhhuyngoctruong@gmail.com>
2025-02-22 01:47:21 -0500

    commit

--- src/main/java/MiniFireForce/Fire.java
+++ src/main/java/MiniFireForce/Fire.java
@@ -1,10 +1,13 @@
 package MiniFireForce;
+import java.time.LocalDateTime;
 
+import javafx.util.Pair;
 public class Fire {
-    private int x;
-    private int y;
-    private int severity;
-    private int time;
+    private float x;
+    private float y;
+    private String severity;
+    private LocalDateTime time;
+    private static int id;
 
     /**
      * Constructor for Fire class
@@ -14,66 +17,80 @@ public class Fire {
      * @param severity severity of the fire
      * @param time     time of the fire
      */
-    public Fire(int x, int y, int severity, int time) {
+    public Fire(float x, float y, String severity, LocalDateTime time) {
         this.x = x;
         this.y = y;
         this.severity = severity;
         this.time = time;
+        id++;
+    }
+
+    public int getID() {
+        return this.id;
     }
 
     /**
      * Get the x-coordinate of the fire
      */
-    public int getX() {
+    public float getX() {
         return x;
     }
 
     /**
-     * Get the y-coordinate of the fire
+     * Get the y-coordinate of the fire.
      */
-    public int getY() {
+    public float getY() {
         return y;
+    }
+    /**
+     * Get the coordinate xy.
+     * @return a point (x,y)
+     */
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public Pair<Float,Float> getXY() {
+        return new Pair(x,y);
     }
 
     /**
      * Get the severity of the fire
      */
-    public int getSeverity() {
+    public String getSeverity() {
         return severity;
     }
 
     /**
      * Get the time of the fire
      */
-    public int getTime() {
+    public LocalDateTime getTime() {
         return time;
     }
 
     /**
      * Set the x-coordinate of the fire
      */
-    public void setX(int newX) {
+    public void setX(float newX) {
         this.x = newX;
     }
 
     /**
      * Set the y-coordinate of the fire
      */
-    public void setY(int newY) {
+    public void setY(float newY) {
         this.y = newY;
     }
 
     /**
      * Set the time of the fire
      */
-    public void setTime(int newTime) {
+    public void setTime(LocalDateTime newTime) {
         this.time = newTime;
     }
 
     /**
      * Set the severity of the fire
      */
-    public void updateSeverity(int newSev) {
+    public void updateSeverity(String newSev) {
         this.severity = newSev;
     }
 }
