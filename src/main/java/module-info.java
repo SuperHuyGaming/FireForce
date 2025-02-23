80fdffc92e50a6653d8b5ed1da6ea67bd3875fb3
u1485085 <nghkhang2005@gmail.com>
2025-02-22 22:37:23 -0700

    implementing gui through app + deleted hello package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module hello {
     requires javafx.fxml;
-    requires javafx.graphics;
     requires java.desktop;
-    requires org.junit.jupiter.api;    // For Swing / AWT
+    requires org.junit.jupiter.api;
+    requires javafx.controls;    // For Swing / AWT
     // ...other requires if needed...
     exports MiniFireForce;    // Export the package containing your main class
 }
